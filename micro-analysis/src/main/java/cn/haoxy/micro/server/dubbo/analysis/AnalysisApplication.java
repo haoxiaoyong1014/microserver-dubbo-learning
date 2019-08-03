ad79e678f37393bdeddf678e649754b1dad900bd
haoxiaoyong1014 <hxyHelloWorld@163.com>
2019-08-03 09:10:15 +0800

    add README.md

---
+++ micro-analysis/src/main/java/cn/haoxy/micro/server/dubbo/analysis/AnalysisApplication.java
@@ -0,0 +1,17 @@
+package cn.haoxy.micro.server.dubbo.analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Haoxy
+ * Created in 2019-08-03.
+ * E-mail:hxyHelloWorld@163.com
+ * github:https://github.com/haoxiaoyong1014
+ */
+@SpringBootApplication
+public class AnalysisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AnalysisApplication.class,args);
+    }
+}
